2f2016afb1b7ec3fc840fa2e9da9c50e85a4504d
goncamateus <mateus.gm@gmail.com>
2026-02-16 21:39:06 -0300

    FEAT: Adding new rewards

--- mjlab_task/stand_env.py
+++ mjlab_task/stand_env.py
@@ -1,13 +1,32 @@
-from mjlab.envs import ManagerBasedRlEnvCfg
+import torch
+from mjlab.entity import Entity
+from mjlab.envs import ManagerBasedRlEnv, ManagerBasedRlEnvCfg
 from mjlab.managers import ObservationGroupCfg
 from mjlab.managers.curriculum_manager import CurriculumTermCfg
 from mjlab.managers.observation_manager import ObservationTermCfg
-from mjlab.sensor.contact_sensor import ContactMatch, ContactSensorCfg
+from mjlab.managers.reward_manager import RewardTermCfg
+from mjlab.managers.scene_entity_config import SceneEntityCfg
+from mjlab.sensor.contact_sensor import ContactMatch, ContactSensor, ContactSensorCfg
 from mjlab.tasks.velocity import mdp
 from mjlab.tasks.velocity.velocity_env_cfg import make_velocity_env_cfg
 from mjlab.utils.noise import UniformNoiseCfg as Unoise
 
 from .robot_cfg import BoosterT1Cfg
+
+
+def torso_height(
+    env: ManagerBasedRlEnv,
+    asset_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
+) -> torch.Tensor:
+    asset: Entity = env.scene[asset_cfg.name]
+    return asset.data.body_com_pos_w[:, asset_cfg.body_ids, 2].sum()
+
+
+def feet_on_ground(env: ManagerBasedRlEnv, sensor_name: str) -> torch.Tensor:
+    sensor: ContactSensor = env.scene[sensor_name]
+    sensor_data = sensor.data
+    assert sensor_data.found is not None
+    return (sensor_data.found > 0).float().sum(dim=1)
 
 
 class T1StandCfgGen:
@@ -41,7 +60,23 @@ class T1StandCfgGen:
         ),
     }
 
-    reward_set = {"pose", "upright"}
+    reward_set = {
+        # Set params and weights
+        "pose",
+        # Set params["asset_cfg"].body_names
+        "upright",
+        "body_ang_vel",
+        # Unchange
+        "soft_landing",
+        "dof_pos_limits",
+        "action_rate_l2",
+        # Set weights
+        "track_linear_velocity",
+        "track_angular_velocity",
+        # New
+        "feet_on_ground",
+        "torso_height",
+    }
 
     def __init__(self):
         self.cfg: ManagerBasedRlEnvCfg = make_velocity_env_cfg()
@@ -50,6 +85,8 @@ class T1StandCfgGen:
         """Configure the scene for the Booster T1 environment."""
         # Set robot
         self.cfg.scene.entities = {"robot": BoosterT1Cfg()}
+        self.cfg.scene.terrain.terrain_type = "plane"
+        self.cfg.scene.terrain.terrain_generator = None
 
         # Add contact sensor for feet (required by standard velocity task observations/rewards)
         self.cfg.scene.sensors = (
@@ -60,8 +97,11 @@ class T1StandCfgGen:
                     pattern=("left_foot_link", "right_foot_link"),
                     entity="robot",
                 ),
+                fields=("force", "torque", "found"),
                 secondary=ContactMatch(mode="body", pattern="terrain"),
                 track_air_time=True,
+                history_length=5,
+                reduce="netforce",
             ),
         )
 
@@ -70,14 +110,53 @@ class T1StandCfgGen:
         self.cfg.viewer.body_name = "base"
         self.cfg.viewer.body_name = "Trunk"
 
-    def setup_rewards(self):
-        """Configure rewards for the Booster T1 environment."""
-        self.cfg.rewards["pose"].params["std_standing"] = {".*": 1}
+    def _setup_pose_reward(self):
+        """Configure the pose reward for the Booster T1 environment."""
+        self.cfg.rewards["pose"].params["std_standing"] = {".*": 10}
         self.cfg.rewards["pose"].params["std_walking"] = {".*": 0}
         self.cfg.rewards["pose"].params["std_running"] = {".*": 0}
         self.cfg.rewards["pose"].weight = 10
+
+    def _setup_body_rewards(self):
+        """Configure body-related rewards for the Booster T1 environment."""
         self.cfg.rewards["upright"].params["asset_cfg"].body_names = ("Trunk",)
         self.cfg.rewards["upright"].weight = 10
+
+        self.cfg.rewards["body_ang_vel"].params["asset_cfg"].body_names = ("Trunk",)
+        self.cfg.rewards["body_ang_vel"].weight = 10
+
+    def _setup_velocity_tracking_rewards(self):
+        """Configure velocity tracking rewards for the Booster T1 environment."""
+        self.cfg.rewards["track_linear_velocity"].weight = -1
+        self.cfg.rewards["track_angular_velocity"].weight = -1
+
+    def _setup_torso_reward(self):
+        """Configure the feet on ground reward for the Booster T1 environment."""
+        self.cfg.rewards["torso_height"] = RewardTermCfg(
+            func=torso_height,
+            weight=10.0,
+            params={
+                "asset_cfg": SceneEntityCfg(
+                    "robot", joint_names=(".*",), body_names=("Trunk",)
+                )
+            },
+        )
+
+    def _setup_feet_on_ground_reward(self):
+        """Configure the feet on ground reward for the Booster T1 environment."""
+        self.cfg.rewards["feet_on_ground"] = RewardTermCfg(
+            func=feet_on_ground,
+            weight=1.0,
+            params={"sensor_name": "feet_ground_contact"},
+        )
+
+    def setup_rewards(self):
+        """Configure rewards for the Booster T1 environment."""
+        self._setup_pose_reward()
+        self._setup_body_rewards()
+        self._setup_velocity_tracking_rewards()
+        self._setup_torso_reward()
+        self._setup_feet_on_ground_reward()
         self.cfg.rewards = {key: self.cfg.rewards[key] for key in self.reward_set}
 
     def choose_observation(self):
--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import argparse
 import subprocess
 import sys
-import time
 from pathlib import Path
 
 
